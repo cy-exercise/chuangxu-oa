b8f072e6a6c7928ad64f88479ef1d9df17291dd1
chen <18725975901@163.com>
2019-04-09 05:38:36 +0800

    a

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -7,8 +7,7 @@
       </router-link>
     </mt-header>
     <div class="order-item">
-      <div class="image">
-        <div class="image-item"></div>
+      <div class="logo">
       </div>
       <div>论文润色</div>
       <div>￥3409.00</div>
@@ -23,12 +22,15 @@
         </div>
       </div>
     </div>
-    <div class="call-phone">
-      <div class="call-people">
-        <img src="" alt="">
-        <div>Kimberly Hernandez</div>
-        <div>电话：13423453431</div>
+    <div class="call-container">
+      <div class="call-content">
+        <div class="call-people">
+          <img src="" alt="">
+          <div>Kimberly Hernandez</div>
+          <div>电话：13423453431</div>
+        </div>
       </div>
+
       <div class="call-button"></div>
     </div>
   </div>
@@ -50,6 +52,7 @@
   .mint-header {
     background: #EDEDED;
     color: #515151;
+    font-size: 16px;
   }
   .order-item {
     height: 285px;
@@ -68,12 +71,22 @@
     background: #F8F8F8;
     border-radius: 4px;
   }
-  .image-item {
-    width:120px;
-    height:120px;
+  .logo {
+    width:60px;
+    height:60px;
     background:rgba(40,178,254,1);
-    border-radius: 60px;
+    border-radius: 30px;
     margin: 0 auto;
+  }
+  .call-container {
+    height: 200px;
+    line-height: 200px;
+    width: 100%;
+    background: #EDEDED;
+  }
+  .call-content {
+    height: 180px;
+    /*background: white;*/
   }
 
 </style>
